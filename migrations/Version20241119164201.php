2b6557671d637c0cfb975a43822bf4ae6e813f50
guerincharly@sfr.fr <lannister135@gmail.com>
2024-11-19 17:46:03 +0100

    Change database driver from PostgreSQL to MySQL and update migration filename

--- migrations/Version20241119164201.php
+++ migrations/Version20241119164201.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241119163815 extends AbstractMigration
+final class Version20241119164201 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,11 +21,16 @@ final class Version20241119163815 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE contact_message (id INT AUTO_INCREMENT NOT NULL, titre VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE animal (id INT AUTO_INCREMENT NOT NULL, race_id INT DEFAULT NULL, name VARCHAR(50) NOT NULL, INDEX IDX_6AAB231F6E59D40D (race_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE animal ADD CONSTRAINT FK_6AAB231F6E59D40D FOREIGN KEY (race_id) REFERENCES race (id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE contact_message');
+        $this->addSql('ALTER TABLE animal DROP FOREIGN KEY FK_6AAB231F6E59D40D');
+        $this->addSql('DROP INDEX IDX_6AAB231F6E59D40D ON animal');
+        $this->addSql('DROP TABLE animal');
     }
 }
