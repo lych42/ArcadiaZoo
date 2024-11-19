c7f5c36ebc35c3abed2b5c1150084ead8991f250
guerincharly@sfr.fr <lannister135@gmail.com>
2024-11-19 19:07:48 +0100

    Update MySQL version to 10.4.32 and modify entity ID generation strategy to 'IDENTITY'

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,7 +11,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 class User implements UserInterface
 {
     #[ORM\Id]
-    #[ORM\GeneratedValue]
+    #[ORM\GeneratedValue(strategy: 'IDENTITY')]
     #[ORM\Column]
     private ?int $id = null;
 
